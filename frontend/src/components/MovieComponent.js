5fda409c5705c64b647eb7616ff61931a8d20816
Victoria Kee <victoriakee95@gmail.com>
2022-03-04 15:44:00 -0600

    replace weekly movies with components

--- frontend/src/components/MovieComponent.js
+++ frontend/src/components/MovieComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Movie({movie}) {
     return (
         <div>
-            <img src={movie.image} alt={movie.title} width="100"/>
+            <img src={movie.image} alt={movie.title}/>
         </div>
     )
 }
--- frontend/src/components/WeeklyMoviesComponent.js
+++ frontend/src/components/WeeklyMoviesComponent.js
@@ -6,7 +6,7 @@ export default function WeeklyMovies() {
     const movieElements = movies.map(movie => {
       return (
         <div className="col-md-4 text-center align-self-center">
-          <Movie />
+          <Movie movie={movie} key={movie.id} />
         </div>
       )
     })
@@ -22,38 +22,7 @@ export default function WeeklyMovies() {
             </div>
             {/*end of title*/}
             <div className="row" id="weekly-movies-tri">
-                <div className="col-md-4 text-center align-self-center">
-                    <img src="https://m.media-amazon.com/images/I/A1Vmrrc2S+L._SL1500_.jpg" alt="Rosemary's Baby"/>
-                </div>
-                <div className="col-md-4 text-center align-self-center">
-                    <img src="https://m.media-amazon.com/images/M/MV5BYzY0ZjJlNmMtMGU3NC00Yjk3LTk0N2ItMDNlMDVhZjA4OTFmXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" alt="The Return of the Living Dead"/>
-                </div>
-                <div className="col-md-4 text-center align-self-center">
-                    <img src="https://m.media-amazon.com/images/I/81UoLziAh8L._SY445_.jpg" alt="Behind the Mask"/>
-                </div>
-            </div>
-            <div className="row" id="weekly-movies-carousel">
-                <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-inner">
-                      <div className="carousel-item active">
-                        <img src="https://m.media-amazon.com/images/I/A1Vmrrc2S+L._SL1500_.jpg" className="d-block w-50 m-auto h-auto" alt="..."/>
-                      </div>
-                      <div className="carousel-item">
-                        <img src="https://m.media-amazon.com/images/M/MV5BYzY0ZjJlNmMtMGU3NC00Yjk3LTk0N2ItMDNlMDVhZjA4OTFmXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" className="d-block w-50 m-auto h-auto" alt="..."/>
-                      </div>
-                      <div className="carousel-item">
-                        <img src="https://m.media-amazon.com/images/I/81UoLziAh8L._SY445_.jpg" className="d-block w-50 m-auto h-auto" alt="..."/>
-                      </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span className="visually-hidden">Next</span>
-                    </button>
-                  </div>
+                {movieElements}
             </div>
         </div>
     </section>
