93ae35920a08da943a672a87873053ab9359f21d
Victoria Kee <victoriakee95@gmail.com>
2022-03-28 15:34:05 -0500

    create err message for no movies

--- frontend/src/routes/SearchPage.js
+++ frontend/src/routes/SearchPage.js
@@ -20,9 +20,10 @@ export default function SearchPage() {
         setSearchTerm(value);
         
     }
+
     
-    const movieElements = moviesData.map(movie => {return <Movie key={movie.id} movie={movie} />})
-    const movieRow = <div className='row'>{movieElements}</div>
+    const movieElements = moviesData.map(movie => {return <Movie key={movie.id} movie={movie} />}) 
+    const movieRow = moviesData.length >= 1 ? <div className='row'>{movieElements}</div> : <div className='text-muted text-center search-error'><h1 className='text-blood'>Oooh, that's scary.</h1><p>We couldn't find that movie.</p></div>
 
 
     return (
