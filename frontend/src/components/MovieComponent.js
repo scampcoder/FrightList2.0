04bc6182b73331524761e08b1049aa4170c82350
Victoria Kee <victoriakee95@gmail.com>
2022-03-28 15:53:39 -0500

    set movie to be link + style link

--- frontend/src/components/MovieComponent.js
+++ frontend/src/components/MovieComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Movie({movie}) {
     return (
-        <img className="searched-movie col-2 py-3" src={movie.image} alt={movie.title}/>
+        <Link to="/" className='col-xs-12 col-sm-6 col-lg-3 col-xl-2 py-3'><img className="col-12 searched-movie" src={movie.image} alt={movie.title}/></Link>
     )
 }
