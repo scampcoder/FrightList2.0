13e7290b873c9674a40224f9bed99420e83e7b36
Victoria Kee <victoriakee95@gmail.com>
2022-04-18 18:25:34 -0500

    initial styling for lists pg

--- frontend/src/components/MovieComponent.js
+++ frontend/src/components/MovieComponent.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Movie({movie, type}) {
-    const styling = type === "searched-movie" ? "col-12 searched-movie" : "list-slide"
+    let styling = "";
+    if(type === "searched-movie"){
+        styling = "col-12 searched-movie"
+    } else if(type === "slide") {
+        styling = "list-slide"
+    } else {
+        styling = "col-3 m-2 list-movie"
+    }
     return (
         <Link to={`/${movie.title}`} className='col-xs-12 col-sm-6 col-lg-3 col-xl-2 py-3'><img className={styling} src={movie.image} alt={movie.title}/></Link>
     )
--- frontend/src/routes/MyListsComponent.js
+++ frontend/src/routes/MyListsComponent.js
@@ -1,11 +1,37 @@
 import React from "react";
 import Navbar from "../components/NavbarComponent";
+import Movie from '../components/MovieComponent'
+import userData from '../dummyUserData.js'
 
 export default function MyLists() {
+    let watchList = userData[0].lists.watchList;
+    let previouslyWatched = userData[0].lists.previouslyWatched;
+
+    function listComponent(movies) {
+        return (
+            <div>
+                {movies.map(movie => {return <Movie key={movie.id} movie={movie} type="list-movie"  />})}
+            </div>
+        )
+    }
+
     return (
         <div>
             <Navbar />
-            <h1>LISTS DATA TO GO HERE</h1>
+            <div className="container mt-4">
+            <section className="row">
+                <div className="col-6 text-center">
+                    <i className="fas fa-skull d-lg-block text-white"></i>
+                    <h1 className="text-light">Watch List</h1>
+                    {listComponent(watchList)}
+                </div>
+                <div className="col-6 text-center">
+                    <i className="fas fa-skull-crossbones d-lg-block text-white"></i>
+                    <h1 className="text-light">Previously Watched</h1>
+                    {listComponent(previouslyWatched)}
+                </div>
+            </section>
+            </div>
         </div>
         
     )
